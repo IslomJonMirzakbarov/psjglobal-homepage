c7dcb5cfd7d0c069976db7520e8adf13795e1708
Ivan Saldano <ivanms1@gmail.com>
2021-07-07 16:35:51 +0900

    css teawks

--- src/pages/Home/Roadmap/YearDisplay/index.tsx
+++ src/pages/Home/Roadmap/YearDisplay/index.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Button from "@/components/Button";
+
+import useIsMobile from "@/hooks/useIsMobile";
+
+import RoundEmptyIcon from "@/assets/icons/round-empty.svg";
+import RoundFilledIcon from "@/assets/icons/round-filled.svg";
 
 import styles from "./YearDisplay.module.scss";
+
+type Page = "first" | "second";
+
+function getEvents(page: Page, events: any[]) {
+  if (page === "first") {
+    return events.slice(0, 6);
+  }
+
+  return events.slice(6);
+}
 
 interface YearDisplay {
   year: {
@@ -9,15 +26,44 @@ interface YearDisplay {
 }
 
 function YearDisplay({ year }: YearDisplay) {
+  const [page, setPage] = useState<Page>("first");
+
+  const isMobile = useIsMobile();
+
+  useEffect(() => {
+    setPage("first");
+  }, [year]);
+
+  const isMoreThanSix = year.events.length > 6;
+
   return (
     <div className={styles.YearDisplay}>
-      {year.events.map((event, index) => (
-        <div key={`${event.title}_${index}`} className={styles.Event}>
-          <p className={styles.Month}>{event?.month}</p>
-          <p className={styles.Title}>{event?.title}</p>
-          <p className={styles.Subtitle}>{event?.subtitle}</p>
+      <div className={styles.EventsContainer}>
+        {isMoreThanSix && !isMobile
+          ? getEvents(page, year.events).map((event, index) => (
+              <div key={`${event.title}_${index}`} className={styles.Event}>
+                <p className={styles.Month}>{event?.month}</p>
+                <p className={styles.Title}>{event?.title}</p>
+                <p className={styles.Subtitle}>{event?.subtitle}</p>
+              </div>
+            ))
+          : year.events.map((event, index) => (
+              <div key={`${event.title}_${index}`} className={styles.Event}>
+                <p className={styles.Month}>{event?.month}</p>
+                <p className={styles.Title}>{event?.title}</p>
+                <p className={styles.Subtitle}>{event?.subtitle}</p>
+              </div>
+            ))}
+      </div>
+      {isMoreThanSix && !isMobile && (
+        <div>
+          {["first", "second"].map((p) => (
+            <Button key={p} type="button" onClick={() => setPage(p)} noStyle>
+              {page === p ? <RoundFilledIcon /> : <RoundEmptyIcon />}
+            </Button>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 }
