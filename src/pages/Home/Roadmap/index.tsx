63d0e424bb437b5773f878f4ff5c2e3c20359e7e
Ivan Saldano <ivanms1@gmail.com>
2021-07-08 09:58:27 +0900

    add title to roadmap, remove placeholder image

--- src/pages/Home/Roadmap/index.tsx
+++ src/pages/Home/Roadmap/index.tsx
@@ -3,10 +3,17 @@ import React from "react";
 import YearSelector from "./YearSelector";
 
 import styles from "./Roadmap.module.scss";
+import { Trans } from "@lingui/react";
 
 function Roadmap() {
   return (
     <div className={styles.Roadmap}>
+      <p className={styles.PageTitle}>
+        <Trans id="06 Roadmap" />
+      </p>
+      <p className={styles.PageSubtitle}>
+        <Trans id="Explore our white paper to learn more about CONUN as a business, our motivations, and our current and future products." />
+      </p>
       <YearSelector />
     </div>
   );
