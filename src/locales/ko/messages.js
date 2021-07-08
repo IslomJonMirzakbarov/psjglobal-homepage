100aff7748a9e2354f3e07583a03abdfdbb3a56e
Jungmin Lee <mina.lee066@gmail.com>
2021-07-08 18:38:39 +0900

    Korean translation added

--- src/locales/ko/messages.js
+++ src/locales/ko/messages.js
@@ -1,10 +1,193 @@
 /*eslint-disable*/ module.exports = {
   messages: {
+    "01 About CONUN": "CONUN 소개",
+    "01 What We Do": "01 What We Do",
+    "02 Distributed Super Computing": "분산 슈퍼 컴퓨팅",
+    "03 DECENTRALIZED FINANCE": "탈중앙화 금융",
+    "03 Decentralized Finance": "탈중앙화 금융",
+    "04 Get Started": "시작하기",
+    "05 White Paper": "화이트페이퍼",
+    "06 Roadmap": "로드맵",
+    "07 Announcements": "공지사항",
+    "A peer-to-peer, decentralized  file sharing service built on our blockchain network.":
+      "CONUN의 블록 체인 네트워크에 구축 된 P2P, 분산 파일 공유 서비스를 제공합니다",
+    "Analyze for CONUN P2P platform":
+      "CONUN P2P platform 분석 및 설계 Peer to Peer platform 아키텍처를 분석 및 설계합니다",
+    "Apply for blockchain and task processing based on docker task":
+      "Docker 기반의 task processing 및 블록체인 연동을 구현합니다",
+    "Apply for blockchain(Ethereum test-net: ropsten) and distributed processing platform based on docker task":
+      "분산 처리 플랫폼(docker 기반) 구축 및 블록체인(ethernum test-net:ropsten) 연동",
+    "Apply for network core and trading system in BTC, ETH, XRP, QTUM etc":
+      "비트코인/이더리움/리플/퀀텀 등 각종 네트워크 코어 및 거래시스템을 적용합니다",
+    Apr: "Apr",
+    "Architect for smartcontrat and mining algorithm for managing trading":
+      "거래트랜잭션을 관리하는 스마트 컨트랙트와 마이닝 알고리즘을 구현합니다.",
+    "Based on desktop grid computing technology, CONUN's Distributed Super Computing Platform operates by agreement between participants and users that supports a horizontal revenue ecosystem. Participants agree to share available space with users, and users access the available space for their personal computing needs - thus creating a mutually beneficial relationship.":
+      "사용의 용이성과 효율성에 중점을 둔 CONUN의 목표는 자사의 플랫폼을 온 / 오프라인 서비스와 연결하여 프로젝트에서 프로젝트로 이어지는 개발을 끊임없이 꿈꾸고 만들어서 사용자에게 광범위한 이점을 제공하는 것입니다",
+    Blogs: "블로그",
+    "Bridge Swap": "브릿지 스왑",
+    "CONUN Services: CONUN Decentralized Data Storage Infrastructure SaaS. CONUN Drive Smart Contract":
+      "CONUN Services: CONUN Decentralized Data Storage Infrastructure SaaS. CONUN Drive Smart Contract",
+    "CONUN TOKEN SWAP": "코넌 토큰 스왑",
+    "CONUN is a blockchain-based distributed supercomputing and resource sharing platform. Together, the participants of our network forms a high-performance resource-sharing service.":
+      "CONUN is a blockchain-based distributed supercomputing and resource sharing platform. Together, the participants of our network forms a high-performance resource-sharing service.",
+    "CONUN p2p protocol is designed for delivering a secure network between trustless users. Security protocols":
+      "CONUN P2P 탈중앙 네트워크에서의 서로를 신뢰할 수 있는 강력한 보안을 제공하도록 설계합니다.",
+    "CONUN’s projects are based on blockchain technology, making them more secure and also giving you more freedom. Currently we have our wallet, drive, and coin available for public use.":
+      "CONUN’s projects are based on blockchain technology, making them more secure and also giving you more freedom. Currently we have our wallet, drive, and coin available for public use.",
+    "CTM Front-End": "CTM 프런트앤드",
+    "Complete CONUN DECENTRALIZED NETWORK":
+      "코넌 탈중앙 네트워크 CONUN DECENTRALIZED NETWORK 서비스 개발완료",
+    "Complete CONUN Network SDKs and APIsn":
+      "코넌 네트워크 SDK 와 API CONUN Network SDKs and APIs 서비스 출시",
+    "Connecting on Universal Networks": "Connecting on Universal Networks",
+    "Contact Us": "연락처",
+    "Conun Wallet Web Extension": "Conun Wallet Web Extension",
+    "Crowdfunding for pre-sale":
+      "개발을 위한 재단기금모금을 위해 사전판매형식의 크라우드펀딩을 진행합니다",
+    "DappStore is a platform, which lists all popular dApps. Discover the innovative world of blockchain, plus explore the many decentralized applications in our platform":
+      "코넌의 디앱스토어에서는 모든 인기있는 dApp을 나열하는 플랫폼입니다. 혁신적인 블록체인 세계를발견하고 당사 플랫폼의 많은 분산 애플리케이션 경험을 제공할 것입니다",
+    Dec: "Dec",
+    "Decentralized finance operates through the decentralized blockchain on a distributed public ledger, it makes all transactions transparent and trackable. With peer-to peer exchanges it eliminates the middle-man present in centralized finance and allows for more control over personal information and of finances.":
+      "Decentralized finance operates through the decentralized blockchain on a distributed public ledger, it makes all transactions transparent and trackable. With peer-to peer exchanges it eliminates the middle-man present in centralized finance and allows for more control over personal information and of finances.",
+    "Develop CONUN's digital currency exchange platform 'Alpha version' for trading digital currency":
+      "Develop CONUN's digital currency exchange platform 'Alpha version' for trading digital currency",
+    "Develop CONUN's digital currency exchange platform for trading digital currency":
+      "Develop CONUN's digital currency exchange platform for trading digital currency",
+    "Develop client server for digital exchange":
+      "Develop client server for digital exchange",
+    "Development CONUN DappStore": "Development CONUN DappStore",
+    "Development CONUN P2P core": "Development CONUN P2P core",
+    "Development CONUN P2P network": "Development CONUN P2P network",
+    "Development CONUN digital currency exchange alpha version platform":
+      "Development CONUN digital currency exchange alpha version platform",
+    "Development CONUN mobile application":
+      "Development CONUN mobile application",
+    "Development CONUN platform core & wallet":
+      "Development CONUN platform core & wallet",
+    "Development distributed network architecture (C/S platform)":
+      "Development distributed network architecture (C/S platform)",
+    "Development distributed network architecture (multi-node processing)":
+      "Development distributed network architecture (multi-node processing)",
+    "Development distributed storage(IPFS)":
+      "Development distributed storage(IPFS)",
+    "Development for CONUN mobile platform for android and iOS version":
+      "Development for CONUN mobile platform for android and iOS version",
+    "Development for client interface (manager)":
+      "Development for client interface (manager)",
+    "Development for total control web version management system in PC and mobile 'CONUN orchestration'":
+      "Development for total control web version management system in PC and mobile 'CONUN orchestration'",
+    "Development project utility and portal server for CONUN market":
+      "Development project utility and portal server for CONUN market",
+    "Download Products": "Download Products",
+    "Easily swap, exchange, and buy with CON coin through our wallet application.":
+      "Easily swap, exchange, and buy with CON coin through our wallet application.",
+    "Explore our white paper to learn more about CONUN as a business, our motivations, and our current and future products.":
+      "Explore our white paper to learn more about CONUN as a business, our motivations, and our current and future products.",
+    Feb: "Feb",
     Github: "Github",
-    Logo: "Lugo",
-    News: "Noticias",
+    "Implement for Client / Server platform network architecture":
+      "Implement for Client / Server platform network architecture",
+    "Implement for client application program":
+      "Implement for client application program",
+    Jan: "Jan",
+    Jobs: "Jobs",
+    Jul: "Jul",
+    Jun: "Jun",
+    "Launch CONUN Blockchain Explorer Testnet":
+      "Launch CONUN Blockchain Explorer Testnet",
+    "Launch CONUN Blockchain Middleware Testnet":
+      "Launch CONUN Blockchain Middleware Testnet",
+    "Launch CONUN Blockchain Testnet": "Launch CONUN Blockchain Testnet",
+    "Launch beta version": "Launch beta version",
+    "Launch cryptocurrency exchange service":
+      "Launch cryptocurrency exchange service",
+    "Launch distributed network (client/server platform) Alpha version in GitHub and pre-trial":
+      "Launch distributed network (client/server platform) Alpha version in GitHub and pre-trial",
+    "Launch distributed network (peer to peer) Alpha version in Github and pre-trial":
+      "Launch distributed network (peer to peer) Alpha version in Github and pre-trial",
+    "Launch distributed network P2P platform":
+      "Launch distributed network P2P platform",
+    "Launch distributed project beta version test":
+      "Launch distributed project beta version test",
+    "Launching CONUN business": "Launching CONUN business",
+    "Launching the CONUN membership service and CONUN Resource Monitoring allows you to configure a cost estimate and income estimate that fits your unique business or personal needs with CONUN platform":
+      "Launching the CONUN membership service and CONUN Resource Monitoring allows you to configure a cost estimate and income estimate that fits your unique business or personal needs with CONUN platform",
+    "Learn More": "Learn More",
+    "Make project for CONUN platform Test application in science circulation, deep learning, big data":
+      "Make project for CONUN platform Test application in science circulation, deep learning, big data",
+    "Make projects for CONUN platform Test : science circulation / deep learning / big data":
+      "Make projects for CONUN platform Test : science circulation / deep learning / big data",
+    "Make public Alpha version for c/s platform":
+      "Make public Alpha version for c/s platform",
+    "Make public CONUN's core service and client wallet in data network":
+      "Make public CONUN's core service and client wallet in data network",
+    "Make the architecture for Peer to Peer platform":
+      "Make the architecture for Peer to Peer platform",
+    "Make transportation & distributed data storage management in application":
+      "Make transportation & distributed data storage management in application",
+    "Make virtual machine system for personal PC app UI/UX":
+      "Make virtual machine system for personal PC app UI/UX",
+    Mar: "Mar",
+    May: "May",
+    "Network Core for exchange platform": "Network Core for exchange platform",
+    Nov: "Nov",
+    Oct: "Oct",
+    "Ongoing project and company updates related to CONUN.":
+      "Ongoing project and company updates related to CONUN.",
+    "Open CONUN's whitepaper describing technical architecture and aim in details":
+      "Open CONUN's whitepaper describing technical architecture and aim in details",
+    "Organize distributed computing system for CONUN's client activities monitoring system":
+      "Organize distributed computing system for CONUN's client activities monitoring system",
+    "Our new cryptocurrency manager keeps your existing wallets together securely.":
+      "Our new cryptocurrency manager keeps your existing wallets together securely.",
+    "PC Application UI/UX/Virtual Machine":
+      "PC Application UI/UX/Virtual Machine",
+    "Planning CONUN digital currency exchange":
+      "Planning CONUN digital currency exchange",
+    "Planning business goal and technology strategy of CONUN":
+      "Planning business goal and technology strategy of CONUN",
+    "Platform development": "Platform development",
+    "Privacy Policy": "Privacy Policy",
+    "Proceed CONUN core(peer to peer platform)":
+      "Proceed CONUN core(peer to peer platform)",
+    "Proceed CONUN smart contract & mining algorithm":
+      "Proceed CONUN smart contract & mining algorithm",
+    "Proceed crowdfunding in pre-sale for launching the company foundation":
+      "Proceed crowdfunding in pre-sale for launching the company foundation",
+    "Proceed exchange beta version test": "Proceed exchange beta version test",
+    "Proceed job scheduler, task allocation process":
+      "Proceed job scheduler, task allocation process",
+    "Proceed network consists of peer to peer platform":
+      "Proceed network consists of peer to peer platform",
+    "Proceed research and development for CONUN's platform CONUN":
+      "Proceed research and development for CONUN's platform CONUN",
+    "Release CONUN Blockchain Explorer Mainnet":
+      "Release CONUN Blockchain Explorer Mainnet",
+    "Release CONUN Blockchain Mainnet ": "Release CONUN Blockchain Mainnet ",
+    "Release CONUN membership service": "Release CONUN membership service",
+    "Release CONUN whitepaper": "Release CONUN whitepaper",
     Roadmap: "Hoja de Ruta",
-    Team: "Equipo",
-    Whitepaper: "Papel",
+    Sep: "Sep",
+    "Share. Earn. Explore": "Share. Earn. Explore",
+    "Staring to manage CONUN algorithm finding key version":
+      "Staring to manage CONUN algorithm finding key version",
+    "Start digital exchange service": "Start digital exchange service",
+    "Starting to test 'CONUN blockchain network service version 'Halla'. We pick the beautiful mountain name in Jeju island as a CONUN's first release service name":
+      "Starting to test 'CONUN blockchain network service version 'Halla'. We pick the beautiful mountain name in Jeju island as a CONUN's first release service name",
+    "Term and Conditions": "Term and Conditions",
+    "Test Beta version for managing digital exchange. Making the core and trading system and security enforcement":
+      "Test Beta version for managing digital exchange. Making the core and trading system and security enforcement",
+    "The first release of CONUN blockchain main services 'Halla - Release'. Complete CONUN DRIVE":
+      "The first release of CONUN blockchain main services 'Halla - Release'. Complete CONUN DRIVE",
+    "Through CONUN Network SDKs, organizations can efficiently integrate their business into the CONUN Blockchain Network. With CONUN provided APIs, organizations can implement their own business applications":
+      "Through CONUN Network SDKs, organizations can efficiently integrate their business into the CONUN Blockchain Network. With CONUN provided APIs, organizations can implement their own business applications",
+    "Universal Resource Sharing": "Universal Resource Sharing",
+    "Update CONUN p2p network traffic": "Update CONUN p2p network traffic",
+    "With the idle processing powers of personal computers and smartphones CONUN provides a horizontal, distributed computing power sharing platform that is open to all application requesters and operators who need computing power. With focus on ease-of use and efficiency CONUN’s goal is to link our platform with on and offline services to provide users with broad benefits as we continue to dream and create from project to project. ":
+      "With the idle processing powers of personal computers and smartphones CONUN provides a horizontal, distributed computing power sharing platform that is open to all application requesters and operators who need computing power. With focus on ease-of use and efficiency CONUN’s goal is to link our platform with on and offline services to provide users with broad benefits as we continue to dream and create from project to project. ",
+    "connect with manager": "connect with manager",
+    "conun finance": "conun finance",
+    "explore drive": "explore drive",
   },
 };
