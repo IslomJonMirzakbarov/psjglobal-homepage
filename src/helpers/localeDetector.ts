c2913ced70af46fff38083067ee4076dc2d5d847
Ivan Saldano <ivanms1@gmail.com>
2021-07-12 17:38:18 +0900

    fix ts error in detect locale

--- src/helpers/localeDetector.ts
+++ src/helpers/localeDetector.ts
@@ -1,14 +1,16 @@
 import { detect, fromNavigator } from "@lingui/detect-locale";
 
+type Locale = "en" | "ko";
+
+const DEFAULT_LOCALE: Locale = "en";
 const AVAILABLE_LANGUAGES = ["en", "ko"];
 
-const DEFAULT_LOCALE = "en";
-
-function detectLocale() {
+function detectLocale(): Locale {
   if (process.browser) {
     const locale = detect(fromNavigator());
 
     if (locale && AVAILABLE_LANGUAGES.includes(locale)) {
+      // @ts-expect-error
       return locale?.slice(0, 2) || DEFAULT_LOCALE;
     }
 
