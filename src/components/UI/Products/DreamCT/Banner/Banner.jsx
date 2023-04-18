34a5775d373744b3d6bf11ba6ad966b5d898885e
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2023-04-18 17:53:05 +0900

    added new section in dreamct part

--- src/components/UI/Products/DreamCT/Banner/Banner.jsx
+++ src/components/UI/Products/DreamCT/Banner/Banner.jsx
@@ -32,10 +32,40 @@ export default function Banner() {
       <Container className={styles.container}>
         <div className={styles.banner}>
           <div className={styles.content}>
+            <div className={styles.containerAccumulation}>
+              <div className={styles.column}>
+                <h2 className={styles.title}>
+                  쌓이고 <br />
+                  있습니다.
+                </h2>
+                <p className={styles.paragraph}>
+                  회차가 끝날때마다 기부,운영,소각
+                  <br />
+                  10%씩 싸이콘이 쌓입니다.
+                </p>
+              </div>
+              <div className={styles.flexRow}>
+                {[1, 2, 3].map((item) => (
+                  <div key={item} className={styles.column}>
+                    <p className={styles.text}>Text {item}</p>
+                    <d iv className={styles.coinWrapper}>
+                      <p className={styles.number}>Number</p>
+                      <img
+                        src='/icons/coin-icon.svg'
+                        width={20}
+                        height={20}
+                        alt='Coin icon'
+                      />
+                    </d>
+                  </div>
+                ))}
+              </div>
+            </div>
+
             <div className={styles.icon}>
               <Image
-                src="/images/products/dreamct/dreamct-icon.png"
-                alt="Main Icon"
+                src='/images/products/dreamct/dreamct-icon.png'
+                alt='Main Icon'
                 width={226}
                 height={285}
               />
@@ -51,31 +81,31 @@ export default function Banner() {
 
             <div className={styles.navigationLinks}>
               <a
-                href="/dreamct_user_guide_en.pdf"
+                href='/dreamct_user_guide_en.pdf'
                 className={styles.navLink}
-                target="_blank"
-                rel="noopener noreferrer"
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 DreamsCT Guide_en
                 <img
-                  src="/icons/arrow-forward-custom.svg"
+                  src='/icons/arrow-forward-custom.svg'
                   width={22}
                   height={22}
-                  alt="arrow"
+                  alt='arrow'
                 />
               </a>
               <a
-                href="/dreamct_user_guide_ko.pdf"
+                href='/dreamct_user_guide_ko.pdf'
                 className={styles.navLink}
-                target="_blank"
-                rel="noopener noreferrer"
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 DreamsCT Guide_ko
                 <img
-                  src="/icons/arrow-forward-custom.svg"
+                  src='/icons/arrow-forward-custom.svg'
                   width={22}
                   height={22}
-                  alt="arrow"
+                  alt='arrow'
                 />
               </a>
             </div>
