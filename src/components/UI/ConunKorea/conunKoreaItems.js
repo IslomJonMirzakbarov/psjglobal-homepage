39b971d76c3a889deb7a6fc29e29950b6ce8a5e6
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2023-08-17 15:49:53 +0900

    conun-korea links page created

---
+++ src/components/UI/ConunKorea/conunKoreaItems.js
@@ -0,0 +1,44 @@
+const items = [
+  {
+    title: 'LINKS',
+    values: [
+      {
+        label: 'CONUN KOREA | 코넌코리아',
+        path: 'https://conun.io/kr',
+        iconPath: '/images/conun-korea/conun-logo.svg'
+      },
+      {
+        label: 'Metacon | 메타콘',
+        path: 'https://conun.io/kr/products/metacon',
+        iconPath: '/images/conun-korea/metacon-logo.svg'
+      },
+      {
+        label: 'Metacon Download | 메타콘 다운로드',
+        path: 'https://play.google.com/store/apps/details?id=com.metacon&pli=1',
+        iconPath: '/images/conun-korea/play-store.svg'
+      },
+      {
+        label: 'Metacon Download | 메타콘 다운로드',
+        path: 'https://apps.apple.com/kr/app/metacon/id6450976505',
+        iconPath: '/images/conun-korea/app-store.svg'
+      },
+      {
+        label: 'WORLD ART DEXPO | 월드아트덱스포',
+        path: 'https://dexpo.world/',
+        iconPath: '/images/conun-korea/wads.svg'
+      },
+      {
+        label: 'YouTube | 유튜브',
+        path: 'https://www.youtube.com/@conun2603',
+        iconPath: '/images/conun-korea/youtube.svg'
+      },
+      {
+        label: 'CONUN SWAP | 코넌스왑',
+        path: 'https://swap.conun.io/',
+        iconPath: '/images/conun-korea/conun-logo.svg'
+      }
+    ]
+  }
+]
+
+export { items }
---
+++ src/components/UI/ConunKorea/index.js
@@ -0,0 +1,50 @@
+import { Container } from '@mui/material'
+import styles from './style.module.scss'
+import { NextArrow } from 'components/UI/Icons'
+import useTranslation from 'next-translate/useTranslation'
+import { items } from 'components/UI/ConunKorea/conunKoreaItems'
+
+export default function ConunKorea() {
+  const { t } = useTranslation('common')
+
+  return (
+    <Container>
+      <div className={styles.titleDiv}>
+        <img
+          src='/images/conun-korea/conun-korea.svg'
+          width={92}
+          height={46}
+          alt=''
+        />
+        <p>@conunkorea</p>
+        <h1 className={styles.snsTitle}>CONUN KOREA</h1>
+      </div>
+      <div className={styles.guide}>
+        <div className={styles.list}>
+          <div className={styles.items}>
+            {items.map((item) => (
+              <div className={styles.item} key={item.title}>
+                <h2>{item.title}</h2>
+                <div className={styles.itemValues}>
+                  {item.values.map((data) => (
+                    <a
+                      href={data.path}
+                      target='_blank'
+                      key={data.label}
+                      rel='noreferrer'
+                    >
+                      <span className={styles.labelSpan}>
+                        <img src={data.iconPath} alt='' /> {data.label}
+                      </span>{' '}
+                      <NextArrow />
+                    </a>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
